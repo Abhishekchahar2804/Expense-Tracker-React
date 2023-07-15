5d8c5886d4641d7cd7ddc23d953ba744fd631164
Abhishek Chahar <abhishekchahar2804@gmail.com>
2023-07-15 09:19:26 +0530

    starting the expense tracker by react

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,11 @@
+function ExpenseItem(){
+    return (
+        <div>
+            <div>July 15 2023</div>
+            <div>Electricity Bill</div>
+            <div>$30.00</div>
+        </div>
+    );
+}
+
+export default ExpenseItem;
