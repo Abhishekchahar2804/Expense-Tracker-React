aa371c3d3e589406db5c7cfb91ccffedc2ca348b
Abhishekchahar2804 <105548044+Abhishekchahar2804@users.noreply.github.com>
2023-07-17 08:59:01 +0530

    adding filter components

--- src/components/Expenses/ExpenseDetails.js
+++ src/components/Expenses/ExpenseDetails.js
@@ -3,17 +3,11 @@ import React,{useState} from 'react';
 import './ExpenseItem.css'
 
 function ExpenseDetails(props) {
-  const [title,setTitle]=useState(props.title)
-
-  const clickHandler=()=>{
-      setTitle('updated');
-  }
 
   return (
     <div className="expense-item__description">
-      <h2>{title}</h2>
+      <h2>{props.title}</h2>
       <div className="expense-item__price">${props.amount}</div>
-      <button onClick={clickHandler}>Change Title</button>
     </div>
   );
 }
--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,13 +1,21 @@
+import React,{useState} from 'react'
 import Card from "../UI/Card";
 import ExpenseItem from "./ExpenseItem";
+import ExpenseFilter from './ExpenseFilter';
 
 import "./Expenses.css";
 
 function Expenses(props) {
   const expenses=props.items
+  const [filteredYear, setFilteredYear] = useState('2020');
+
+  const filterChangeHandler = selectedYear => {
+    setFilteredYear(selectedYear);
+  };
 
   return (
     <Card className="expenses">
+      <ExpenseFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
       <ExpenseItem
         title={expenses[0].title}
         amount={expenses[0].amount}
